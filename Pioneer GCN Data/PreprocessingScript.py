895ecea78d4f94e668aad387b37bd08bc694f6ec
Ganesh Gajavelli <gajavegr@rose-hulman.edu>
2021-08-16 07:34:16 -0400

    now data is good to run on GCN

--- Pioneer GCN Data/PreprocessingScript.py
+++ Pioneer GCN Data/PreprocessingScript.py
@@ -35,12 +35,33 @@ graph_labels_filename = 'GCN_graph_labels.txt'
 node_labels_filename = 'GCN_node_labels.txt'
 
 node_label_list_df = pd.read_csv('node labels.txt',header=None)
+# n=7;
+# mostGroups = 164//n;
+# lastGroup = 164//n + 164%n
 
-node_list = []
+# node_label_dfs = []
+# i = 1
+# multiplier = 1
+# for i in range(1,n+1):
+#     if (i < n):
+#         # for j in range(1*multiplier,mostGroups*multiplier):
+#         temp = [i]*mostGroups
+#         # print(len(temp))
+#     else:
+#         # temp = list(range(1*multiplier,lastGroup*multiplier))
+#         temp = [i]*lastGroup
+#         # print(len(temp))
+#     node_label_dfs.append(pd.DataFrame(temp))
+#     multiplier += 1
 
-node_range = range(1,164)
-for i in node_range:
-    node_list.append(i);
+# node_label_list_df = pd.concat(node_label_dfs,ignore_index=True)
+# print(node_label_list_df)
+
+node_list = list(range(1,164))
+
+# node_range = range(1,164)
+# for i in node_range:
+#     node_list.append(i);
 
 nodes_df = pd.DataFrame(node_list, columns=["nodes"])
 
@@ -69,14 +90,15 @@ graphIndicators = graphIndicator_df['Graph Indicator']
 
 graphIndicatorFrames = [graphIndicators]
 
-graphLabels_df = pd.DataFrame(index=np.arange(164))
-graphLabels_df['Graph Label'] = controlLabel
+# graphLabels_df = pd.DataFrame(index=np.arange(164))
+# graphLabels_df['Graph Label'] = controlLabel
+graphLabels_df = pd.DataFrame([controlLabel],columns=['Graph Label'])
 graphLabels = graphLabels_df['Graph Label']
 graphLabelFrames = [graphLabels]
 
 nodeIterator += 1
 graphIndicator += 1
-
+"""
 for ctrlFile in controlFiles[1:]:
     df1 = pd.read_csv(filedir + '/' + ctrlFile)
     df1['Source'] = df1['Source'] + 164*nodeIterator
@@ -95,15 +117,13 @@ for ctrlFile in controlFiles[1:]:
     graphIndicators1 = graphIndicator_df1['Graph Indicator']
     graphIndicatorFrames.append(graphIndicators1)
 
-    graphLabels_df1 = pd.DataFrame(index=np.arange(164))
-    graphLabels_df1['Graph Label'] = controlLabel
+    graphLabels_df1 = pd.DataFrame([controlLabel],columns=['Graph Label'])
+    # print(graphLabels_df1)
     graphLabels1 = graphLabels_df1['Graph Label']
     graphLabelFrames.append(graphLabels1)
     
     nodeIterator += 1
     graphIndicator += 1
-
-
 
 for pdFile in pdFiles:
     df1 = pd.read_csv(filedir + '/' + pdFile)
@@ -123,13 +143,83 @@ for pdFile in pdFiles:
     graphIndicators1 = graphIndicator_df1['Graph Indicator']
     graphIndicatorFrames.append(graphIndicators1)
 
-    graphLabels_df1 = pd.DataFrame(index=np.arange(164))
-    graphLabels_df1['Graph Label'] = pdLabel
+    # graphLabels_df1 = pd.DataFrame(index=np.arange(164))
+    # graphLabels_df1['Graph Label'] = pdLabel
+
+    graphLabels_df1 = pd.DataFrame([pdLabel],columns=['Graph Label'])
+
     graphLabels1 = graphLabels_df1['Graph Label']
     graphLabelFrames.append(graphLabels1)
     
     nodeIterator += 1
     graphIndicator += 1
+"""
+ctrlFileCounter = 1
+pdFileCounter = 0
+
+for i in range(0,20):
+    
+    if i%2 == 1:
+        if (ctrlFileCounter < len(controlFiles)):
+            ctrlFile = controlFiles[ctrlFileCounter]
+            df1 = pd.read_csv(filedir + '/' + ctrlFile)
+            df1['Source'] = df1['Source'] + 164*nodeIterator
+            df1['Target'] = df1['Target'] + 164*nodeIterator
+            edgeList1 = df1[['Source','Target']]
+            edgeFrames.append(edgeList1)
+
+            df1['Edge_Labels'] = 1
+            edgeLabels1 = df1['Edge_Labels']
+            edgeLabelsFrames.append(edgeLabels1)
+            node_label_frames.append(node_label_list_df)
+            node_id_frames.append(nodes_df)
+
+            graphIndicator_df1 = pd.DataFrame(index=np.arange(164))
+            graphIndicator_df1['Graph Indicator'] = graphIndicator
+            graphIndicators1 = graphIndicator_df1['Graph Indicator']
+            graphIndicatorFrames.append(graphIndicators1)
+
+            graphLabels_df1 = pd.DataFrame([controlLabel],columns=['Graph Label'])
+            # print(graphLabels_df1)
+            graphLabels1 = graphLabels_df1['Graph Label']
+            graphLabelFrames.append(graphLabels1)
+            
+            nodeIterator += 1
+            graphIndicator += 1
+            ctrlFileCounter += 1
+    else:
+        if (pdFileCounter < len(pdFiles)):
+            pdFile = pdFiles[pdFileCounter]
+            df1 = pd.read_csv(filedir + '/' + pdFile)
+            df1['Source'] = df1['Source'] + 164*nodeIterator
+            df1['Target'] = df1['Target'] + 164*nodeIterator
+            edgeList1 = df1[['Source','Target']]
+            edgeFrames.append(edgeList1)
+
+            df1['Edge_Labels'] = 1
+            edgeLabels1 = df1['Edge_Labels']
+            edgeLabelsFrames.append(edgeLabels1)
+            node_label_frames.append(node_label_list_df)
+            node_id_frames.append(nodes_df)
+
+            graphIndicator_df1 = pd.DataFrame(index=np.arange(164))
+            graphIndicator_df1['Graph Indicator'] = graphIndicator
+            graphIndicators1 = graphIndicator_df1['Graph Indicator']
+            graphIndicatorFrames.append(graphIndicators1)
+
+            # graphLabels_df1 = pd.DataFrame(index=np.arange(164))
+            # graphLabels_df1['Graph Label'] = pdLabel
+
+            graphLabels_df1 = pd.DataFrame([pdLabel],columns=['Graph Label'])
+
+            graphLabels1 = graphLabels_df1['Graph Label']
+            graphLabelFrames.append(graphLabels1)
+            
+            nodeIterator += 1
+            graphIndicator += 1
+            pdFileCounter +=1
+
+
 
 edges_dfs = pd.concat(edgeFrames,ignore_index=True)
 edges_dfs.to_csv(a_filename,header=False,index=False)
